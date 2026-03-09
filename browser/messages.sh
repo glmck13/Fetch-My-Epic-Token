fcef5b771e7bf69ad1ed1aa78fce6ae19e463a6f
glmck13 <glmck13@verizon.net>
2026-03-09 09:28:19 -0400

    Update messages.sh

--- browser/messages.sh
+++ browser/messages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Copy 'GetOrganizations' cURL API call, then hit enter when ready..."
+echo "Copy 'GetOrganizations' cURL API call, then hit enter when ready..." >&2
 read x
 
 wl-paste -t text/plain | xargs -0 printf '%b' >curl.sh
